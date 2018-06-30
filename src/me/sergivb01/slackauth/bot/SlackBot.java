55d9bc0cde6d472a715ffee43dae4550a1c220ee
sergivb01 <sergivb01@gmail.com>
2018-06-30 12:46:08 +0200

    now opensrc

--- src/me/sergivb01/slackauth/bot/SlackBot.java
+++ src/me/sergivb01/slackauth/bot/SlackBot.java
@@ -10,78 +10,78 @@ import org.bukkit.Bukkit;
 import java.io.IOException;
 import java.net.Proxy;
 
-public class SlackBot {
-    @Getter private SlackSession session;
-    private Auth instance;
-    private String channel;
+public class SlackBot{
+	@Getter
+	private SlackSession session;
+	private Auth instance;
+	private String channel;
 
-    public SlackBot(String token, String channel, boolean proxy, String proxyURL, int proxyPort) {
-        instance = Auth.getInstance();
-        this.channel = channel;
-        new Thread(()->{ //New thread => i don't want to crash :(
-            init(token, proxy, proxyURL, proxyPort);
-            registerEvents();
-        }).start();
-    }
+	public SlackBot(String token, String channel, boolean proxy, String proxyURL, int proxyPort){
+		instance = Auth.getInstance();
+		this.channel = channel;
+		new Thread(() -> { //New thread => i don't want to crash :(
+			init(token, proxy, proxyURL, proxyPort);
+			registerEvents();
+		}).start();
+	}
 
-    private void init(String token, boolean proxy, String proxyURL, int proxyPort) {
-        if(proxy){ //Using proxy?
-            session = SlackSessionFactory.getSlackSessionBuilder(token).withAutoreconnectOnDisconnection(true).withProxy(Proxy.Type.SOCKS, proxyURL, proxyPort).build();
-        }else {
-            session = SlackSessionFactory.getSlackSessionBuilder(token).withAutoreconnectOnDisconnection(true).build();
-        }
+	private void init(String token, boolean proxy, String proxyURL, int proxyPort){
+		if(proxy){ //Using proxy?
+			session = SlackSessionFactory.getSlackSessionBuilder(token).withAutoreconnectOnDisconnection(true).withProxy(Proxy.Type.SOCKS, proxyURL, proxyPort).build();
+		}else{
+			session = SlackSessionFactory.getSlackSessionBuilder(token).withAutoreconnectOnDisconnection(true).build();
+		}
 
-        try {
-            session.connect();
-        } catch (IOException e) {
-            e.printStackTrace();
-       }
-        sendMessage(instance.getConfig().getString("messages.bot.connected-message")); //Hello World!
-    }
+		try{
+			session.connect();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		sendMessage(instance.getConfig().getString("messages.bot.connected-message")); //Hello World!
+	}
 
-    private void registerEvents(){
-        session.addMessagePostedListener((slackMessagePosted, slackSession) -> {
-            if(!slackMessagePosted.getChannel().getName().equals(channel) || slackMessagePosted.getSender().isBot()){
-                return;
-            }
+	private void registerEvents(){
+		session.addMessagePostedListener((slackMessagePosted, slackSession) -> {
+			if(!slackMessagePosted.getChannel().getName().equals(channel) || slackMessagePosted.getSender().isBot()){
+				return;
+			}
 
-            String message = slackMessagePosted.getMessageContent();
-            String[] args = message.split("\\s+"); //Space
+			String[] args = slackMessagePosted.getMessageContent().split("\\s+"); //Space
 
-            if(args.length > 0){
-                String player = args[1];
-                switch (args[0].toLowerCase()) {
-                    case "auth":
-                        instance.getPlayerListener().setCode(player, "slack");
-                        break;
-                    case "delcode":
-                        instance.getPlayerListener().removeCode(player);
-                        sendMessage(instance.getConfig().getString("messages.bot.code-removed").replace("%player%", player));
-                        break;
-                    case "ban":
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), instance.getConfig().getString("commands.ban").replace("%player%", player));
-                        sendMessage(instance.getConfig().getString("messages.bot.player-banned").replace("%player%", player));
-                        break;
-                    case "unban":
-                    case "pardon":
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), instance.getConfig().getString("commands.unban").replace("%player%", player));
-                        sendMessage(instance.getConfig().getString("messages.bot.player-unbanned").replace("%player%", player));
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
-        instance.getLogger().info("Events registered");
-    }
+			if(args.length > 0){
+				String player = args[1];
+				switch(args[0].toLowerCase()){
+					case "auth":
+						instance.getPlayerListener().setCode(player, "slack");
+						break;
+					case "delcode":
+						instance.getPlayerListener().removeCode(player);
+						sendMessage(instance.getConfig().getString("messages.bot.code-removed").replace("%player%", player));
+						break;
+					case "ban":
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), instance.getConfig().getString("commands.ban").replace("%player%", player));
+						sendMessage(instance.getConfig().getString("messages.bot.player-banned").replace("%player%", player));
+						break;
+					case "unban":
+					case "pardon":
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), instance.getConfig().getString("commands.unban").replace("%player%", player));
+						sendMessage(instance.getConfig().getString("messages.bot.player-unbanned").replace("%player%", player));
+						break;
+					default:
+						break;
+				}
+			}
+		});
+		instance.getLogger().info("Events registered");
+	}
 
-    public void sendMessage(String message){
-        new Thread(()->{
-            SlackChannel slackChannel = session.findChannelByName(channel);
-            session.sendMessage(slackChannel, message);
-            Auth.getInstance().getLogger().info("[Slack] [Debug] Sent \"" + message + "\" to Slack channel.");
-        }).start();
-    }
+	public void sendMessage(String message){
+		new Thread(() -> {
+			SlackChannel slackChannel = session.findChannelByName(channel);
+			session.sendMessage(slackChannel, message);
+			Auth.getInstance().getLogger().info("[Slack] [Debug] Sent \"" + message + "\" to Slack channel.");
+		}).start();
+	}
 
 
 }
--- src/me/sergivb01/slackauth/listener/PlayerListener.java
+++ src/me/sergivb01/slackauth/listener/PlayerListener.java
@@ -8,37 +8,39 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import redis.clients.jedis.Jedis;
 
 import java.util.Random;
 
 public class PlayerListener implements Listener{
-    private Auth instance;
+	private Auth instance;
 
-    public PlayerListener(){
-        instance = Auth.getInstance();
-        Bukkit.getPluginManager().registerEvents(this, instance);
-    }
+	public PlayerListener(){
+		instance = Auth.getInstance();
+		Bukkit.getPluginManager().registerEvents(this, instance);
+	}
 
-    @EventHandler
-    public void onAsyncChat(AsyncPlayerChatEvent event){
-        Player player = event.getPlayer();
-        if(player.hasPermission("auth.require")){
-            if(playerIsAuthed(player)){
-                return;
-            }
-            if(event.getMessage().equals(getPlayerCode(player.getName()))){
-                setAuthed(player);
-                removeCode(player.getName());
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.authed")));
-                event.setCancelled(true);
-            }else{
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.need-auth")));
-                event.setCancelled(true);
-            }
-        }
-    }
+	@EventHandler
+	public void onAsyncChat(AsyncPlayerChatEvent event){
+		Player player = event.getPlayer();
+		if(player.hasPermission("auth.require")){
+			if(playerIsAuthed(player)){
+				return;
+			}
+			if(event.getMessage().equals(getPlayerCode(player.getName()))){
+				setAuthed(player);
+				removeCode(player.getName());
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.authed")));
+				event.setCancelled(true);
+			}else{
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.need-auth")));
+				event.setCancelled(true);
+			}
+		}
+	}
 
     /*@EventHandler //Now using: "auth username" => multilobby support
     public void onPlayerJoin(PlayerJoinEvent event){
@@ -49,162 +51,162 @@ public class PlayerListener implements Listener{
         }
     }*/
 
-    @EventHandler
-    public void onCommand(PlayerCommandPreprocessEvent event){
-        Player player = event.getPlayer();
-        if(player.hasPermission("auth.require")){
-            if(!playerIsAuthed(player)){
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.need-auth")));
-                event.setCancelled(true);
-            }
-        }
-    }
+	@EventHandler
+	public void onCommand(PlayerCommandPreprocessEvent event){
+		Player player = event.getPlayer();
+		if(player.hasPermission("auth.require")){
+			if(!playerIsAuthed(player)){
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.need-auth")));
+				event.setCancelled(true);
+			}
+		}
+	}
 
-    @EventHandler
-    public void onInteractEvent(PlayerInteractEvent event){
-        Player player = event.getPlayer();
+	@EventHandler
+	public void onInteractEvent(PlayerInteractEvent event){
+		Player player = event.getPlayer();
 
-        if(player.hasPermission("auth.require")){
-            if(!playerIsAuthed(player)){
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.need-auth")));
-                event.setCancelled(true);
-            }
-        }
-    }
+		if(player.hasPermission("auth.require")){
+			if(!playerIsAuthed(player)){
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.need-auth")));
+				event.setCancelled(true);
+			}
+		}
+	}
 
-    @EventHandler
-    public void onInvOpen(InventoryOpenEvent event){
-        Player player = (Player) event.getPlayer();
+	@EventHandler
+	public void onInvOpen(InventoryOpenEvent event){
+		Player player = (Player) event.getPlayer();
 
-        if(player.hasPermission("auth.require")){
-            if(!playerIsAuthed(player)){
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.need-auth")));
-                player.closeInventory();
-                event.setCancelled(true);
-            }
-        }
-    }
+		if(player.hasPermission("auth.require")){
+			if(!playerIsAuthed(player)){
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.need-auth")));
+				player.closeInventory();
+				event.setCancelled(true);
+			}
+		}
+	}
 
-    @EventHandler
-    public void onItemClick(InventoryClickEvent event){
-        Player player = (Player) event.getWhoClicked();
+	@EventHandler
+	public void onItemClick(InventoryClickEvent event){
+		Player player = (Player) event.getWhoClicked();
 
-        if(player.hasPermission("auth.require")){
-            if(!playerIsAuthed(player)){
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.need-auth")));
-                player.closeInventory();
-                event.setCancelled(true);
-            }
-        }
-    }
+		if(player.hasPermission("auth.require")){
+			if(!playerIsAuthed(player)){
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("messages.need-auth")));
+				player.closeInventory();
+				event.setCancelled(true);
+			}
+		}
+	}
 
-    private boolean playerIsAuthed(Player player){
-        boolean toReturn;
-        Jedis jedis = null;
-        try {
-            jedis = instance.getPool().getResource();
-            String lastIP = jedis.hget("auth:players", player.getName());
-            toReturn = (lastIP != null && lastIP.equals(player.getAddress().getHostString()));
-            instance.getPool().returnResource(jedis);
-        }finally {
-            if (jedis != null) {
-                jedis.close();
-            }
-        }
-        return toReturn;
-    }
+	private boolean playerIsAuthed(Player player){
+		boolean toReturn;
+		Jedis jedis = null;
+		try{
+			jedis = instance.getPool().getResource();
+			String lastIP = jedis.hget("auth:players", player.getName());
+			toReturn = (lastIP != null && lastIP.equals(player.getAddress().getHostString()));
+			instance.getPool().returnResource(jedis);
+		}finally{
+			if(jedis != null){
+				jedis.close();
+			}
+		}
+		return toReturn;
+	}
 
-    private void setAuthed(Player player){
-        Jedis jedis = null;
-        try {
-            jedis = instance.getPool().getResource();
-            jedis.hset("auth:players", player.getName(), player.getAddress().getHostString());
-            if(instance.getConfig().getBoolean("slack.enabled")){
-                Auth.getSlackBot().sendMessage(instance.getConfig().getString("messages.bot.player-authed")
-                        .replace("%player%", player.getName()));
-            }
-            if(instance.getConfig().getBoolean("discord.enabled")){
-                Auth.getDiscordBot().sendMessage(instance.getConfig().getString("messages.bot.player-authed")
-                        .replace("%player%", player.getName()));
-            }
-            instance.getPool().returnResource(jedis);
-        }finally {
-            if (jedis != null) {
-                jedis.close();
-            }
-        }
-    }
+	private void setAuthed(Player player){
+		Jedis jedis = null;
+		try{
+			jedis = instance.getPool().getResource();
+			jedis.hset("auth:players", player.getName(), player.getAddress().getHostString());
+			if(instance.getConfig().getBoolean("slack.enabled")){
+				Auth.getSlackBot().sendMessage(instance.getConfig().getString("messages.bot.player-authed")
+						.replace("%player%", player.getName()));
+			}
+			if(instance.getConfig().getBoolean("discord.enabled")){
+				Auth.getDiscordBot().sendMessage(instance.getConfig().getString("messages.bot.player-authed")
+						.replace("%player%", player.getName()));
+			}
+			instance.getPool().returnResource(jedis);
+		}finally{
+			if(jedis != null){
+				jedis.close();
+			}
+		}
+	}
 
-    public void setCode(String playerName, String service){
-        new Thread(()->{
-            try {
-                Thread.sleep(instance.getConfig().getInt("server-num"));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            if(getPlayerCode(playerName) != null){
-                instance.getLogger().info("Code for " + playerName + " already exists. Skipping");
-                return;
-            }
+	public void setCode(String playerName, String service){
+		new Thread(() -> {
+			try{
+				Thread.sleep(instance.getConfig().getInt("server-num"));
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+			if(getPlayerCode(playerName) != null){
+				instance.getLogger().info("Code for " + playerName + " already exists. Skipping");
+				return;
+			}
 
-            Random rnd = new Random();
-            int n = 100000 + rnd.nextInt(900000);
-            Jedis jedis = null;
-            try {
-                jedis = instance.getPool().getResource();
-                jedis.hset("auth:codes", playerName, String.valueOf(n));
-                if(service.equals("slack")){
-                    Auth.getSlackBot().sendMessage(instance.getConfig().getString("messages.bot.code-set")
-                            .replace("%player%", playerName)
-                            .replace("%pin%", String.valueOf(n)));
-                }
-                if(service.equals("discord")){
-                    Auth.getDiscordBot().sendMessage(instance.getConfig().getString("messages.bot.code-set")
-                            .replace("%player%", playerName)
-                            .replace("%pin%", String.valueOf(n)));
-                }
-                instance.getPool().returnResource(jedis);
-            }finally {
-                if (jedis != null) {
-                    jedis.close();
-                }
-            }
-        }).start();
-    }
+			Random rnd = new Random();
+			int n = 100000 + rnd.nextInt(900000);
+			Jedis jedis = null;
+			try{
+				jedis = instance.getPool().getResource();
+				jedis.hset("auth:codes", playerName, String.valueOf(n));
+				if(service.equals("slack")){
+					Auth.getSlackBot().sendMessage(instance.getConfig().getString("messages.bot.code-set")
+							.replace("%player%", playerName)
+							.replace("%pin%", String.valueOf(n)));
+				}
+				if(service.equals("discord")){
+					Auth.getDiscordBot().sendMessage(instance.getConfig().getString("messages.bot.code-set")
+							.replace("%player%", playerName)
+							.replace("%pin%", String.valueOf(n)));
+				}
+				instance.getPool().returnResource(jedis);
+			}finally{
+				if(jedis != null){
+					jedis.close();
+				}
+			}
+		}).start();
+	}
 
-    public void removeCode(String playerName){
-        new Thread(()-> {
-            try {
-                Thread.sleep(instance.getConfig().getInt("server-num"));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Jedis jedis = null;
-            try {
-                jedis = instance.getPool().getResource();
-                jedis.hdel("auth:codes", playerName);
-                instance.getPool().returnResource(jedis);
-            } finally {
-                if (jedis != null) {
-                    jedis.close();
-                }
-            }
-        }).start();
-    }
+	public void removeCode(String playerName){
+		new Thread(() -> {
+			try{
+				Thread.sleep(instance.getConfig().getInt("server-num"));
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+			Jedis jedis = null;
+			try{
+				jedis = instance.getPool().getResource();
+				jedis.hdel("auth:codes", playerName);
+				instance.getPool().returnResource(jedis);
+			}finally{
+				if(jedis != null){
+					jedis.close();
+				}
+			}
+		}).start();
+	}
 
-    private String getPlayerCode(String playerName){
-        String toReturn;
-        Jedis jedis = null;
-        try {
-            jedis = instance.getPool().getResource();
-            toReturn = jedis.hget("auth:codes", playerName);
-            instance.getPool().returnResource(jedis);
-        }finally {
-            if (jedis != null) {
-                jedis.close();
-            }
-        }
-        return toReturn;
-    }
+	private String getPlayerCode(String playerName){
+		String toReturn;
+		Jedis jedis = null;
+		try{
+			jedis = instance.getPool().getResource();
+			toReturn = jedis.hget("auth:codes", playerName);
+			instance.getPool().returnResource(jedis);
+		}finally{
+			if(jedis != null){
+				jedis.close();
+			}
+		}
+		return toReturn;
+	}
 
 }
